43999b51200afcd6dc2827438ca7027a0e681146
Prateek Kishore Karnad <trainer.it.ssk@gmail.com>
2023-08-04 04:35:27 -0700

    db diagram changed

--- src/main/java/com/cbt/cbtaug23/Userdetail.java
+++ src/main/java/com/cbt/cbtaug23/Userdetail.java
@@ -11,22 +11,16 @@ public class Userdetail {
     @Id
     @Column(name = "username", nullable = false, length = 50)
     private String username;
-
     @Column(name = "fname", length = 50)
     private String fname;
-
     @Column(name = "lname", nullable = false, length = 50)
     private String lname;
-
     @Column(name = "email", length = 50)
     private String email;
-
     @Column(name = "phone", length = 50)
     private String phone;
-
     @Column(name = "city", length = 50)
     private String city;
-
     @Column(name = "country", length = 50)
     private String country;
 
